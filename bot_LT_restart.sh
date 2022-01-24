f90030a7cd23f740f201e9f60082fbe76b1def6b
Belousov Aleksandr <statazot@mail.ru>
2022-01-24 10:47:50 +0300

    del

--- bot_LT_restart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-kill -9 $(pidof node)
-kill -9 $(pidof python3)
-/bin/sleep 5
-cd /home/belousov/bot_current
-/usr/bin/lt --port 5000 --subdomain miptdlsbot &> /var/log/localtunnel &
-/usr/bin/python3 bot.py &> /var/log/telegrambot &
